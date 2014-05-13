b3ab3552b4b69c24cef6a4d304c8ef1b4f5c018f
elnappo <elnappoo@gmail.com>
2014-05-13 16:37:28 +0200

    change setup.sh for new folder structure

--- init/setup.sh
+++ init/setup.sh
@@ -1,24 +1,40 @@
 #!/usr/bin/env bash
 
-# install homwbrew
-ruby -e "$(curl -fsSL https://raw.github.com/Homebrew/homebrew/go/install)"
-
 # set osx defaults
+echo "[i] Set OS X defaults"
 ./osx
 
+# install homwbrew
+echo "[i] Install Homebrew"
+ruby -e "$(curl -fsSL https://raw.github.com/Homebrew/homebrew/go/install)"
+
 # install brew formula
+echo "[i] Install Brew formula"
 brew bundle Brewfile
 
-export PATH=(brew --prefix coreutils)/libexec/gnubin:$PATH
+# install brew cask apps
+echo "[i] Install brew cask apps"
+./cask
 
 # install oh-my-zsh
+echo "[i] Install oh-my-zsh"
 git clone git://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
 
 # setup zsh as default shell
+echo "[i] Set zsh as default shell"
 chsh -s /usr/local/bin/zsh
+source ~/.zshrc
 
-# install brew cask apps
-./cask
+# linking files
+echo "[i] Linking dotfiles"
+ln -s ../bashrc .bashrc
+ln -s ../zshrc .zshrc
+ln -s ../wgetrc .wgetrc
+ln -s ../vimrc .vimrc
+ln -s ../curlrc .curlrc
 
-echo "[+] Done :)
+# Some stuff
+echo "[i] Doing some stuff"
+mkdir ~/.virtualenvs 
 
+echo "[+] Done :)"
