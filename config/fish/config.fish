fcd0b97dfefaec1dbbb5b6831dc77900afaafdc3
elnappo <elnappoo@gmail.com>
2015-10-25 17:20:26 +0100

    add fish shell

---
+++ config/fish/config.fish
@@ -0,0 +1,8 @@
+# Path to Oh My Fish install.
+set -gx OMF_PATH $HOME/.local/share/omf
+
+# Customize Oh My Fish configuration path.
+#set -gx OMF_CONFIG $HOME/.dotfiles/config/omf
+
+# Load oh-my-fish configuration.
+source $OMF_PATH/init.fish
