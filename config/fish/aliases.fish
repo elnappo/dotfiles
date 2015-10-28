b0faa77abea9ca110d0aff8ad4964ef65bfcc1a0
elnappo <elnappoo@gmail.com>
2015-10-28 13:36:56 +0100

    add fish config

---
+++ config/fish/aliases.fish
@@ -0,0 +1,95 @@
+# Easier navigation
+function ..; cd ..; end
+function ...; cd ../..; end
+function ....; cd ../../..; end
+  
+# Color output
+function grep -d "File pattern searcher"
+    command grep --color=auto $argv; end
+
+function ls -d "Colorized ls"
+    command ls -G $argv; end
+
+function l -d "l"
+    ls -lah $argv; end
+
+function tree -d "List contents of directories in a tree-like format"
+    command tree -C; end
+
+function reload -d "Reload fish config"
+    source ~/.config/fish/config.fish; end
+
+function df -d "Display free disk space"
+    command df -h $argv; end
+
+function ip -d "Get public IP"
+    dig +short myip.opendns.com @resolver1.opendns.com; end
+
+function ips -d "Get all IPs"
+    ifconfig -a | grep -o 'inet6\? \(addr:\)\?\s\?\(\(\([0-9]\+\.\)\{3\}[0-9]\+\)\|[a-fA-F0-9:]\+\)' | awk '{ sub(/inet6? (addr:)? ?/, \"\"); print }'; end
+
+function lscleanup -d "Clean up LaunchServices to remove duplicates in the Open With menu"
+    /System/Library/Frameworks/CoreServices.framework/Frameworks/LaunchServices.framework/Support/lsregister -kill -r -domain local -domain system -domain user; and killall Finder; end
+
+function cleanup -d " Recursively delete .DS_Store"
+    find . -type f -name '*.DS_Store' -ls -delete; end
+
+function emptytrash -d "Empty all Trashs and clear system log"
+    sudo rm -rfv /Volumes/*/.Trashes; sudo rm -rfv ~/.Trash; sudo rm -rfv /private/var/log/asl/*.asl; end
+
+function mergepdf -d "Merge PDF files"
+    System/Library/Automator/Combine\ PDF\ Pages.action/Contents/Resources/join.py $argv; end
+
+function update -d "Update the system"
+    sudo softwareupdate -i -a; brew update; and brew upgrade --all; brew cleanup; brew cask cleanup; git -C ~/.dotfiles pull; end
+
+function afk -d "Lock the screen (when going AFK)"
+    /System/Library/CoreServices/Menu\ Extras/User.menu/Contents/Resources/CGSession -suspend; end
+
+function pubkey -d "Copy my public key to the pasteboard"
+    more ~/.ssh/id_rsa.pub | pbcopy | printf '=> Public key copied to pasteboard.\n'; end
+
+function p8 -d "Ping 8.8.8.8"
+    ping 8.8.8.8; end
+
+function p8 -d "Ping google.de"
+    ping google.de; end
+
+function pwgen -d "Generate saves passwords"
+    command pwgen --capitalize --numerals --symbols --secure 16; end
+
+function wifipass -d "Get password from a saved AP"
+    security find-generic-password -g -D "AirPort network password" -w -a $argv; end
+
+function wifipow -d "Perform a wireless broadcast scan"
+    /System/Library/PrivateFrameworks/Apple80211.framework/Versions/Current/Resources/airport -s; end
+
+function axel -d "Multiple connections wget"
+    command axel -an 10 $argv; end
+
+function ccat -d "Syntax highlighting cat"
+    pygmentize -g $argv; end
+
+function ssh_ignore -d "Ignore ssh config"
+    ssh -F /dev/null $argv; end
+
+function starwars -d "Play Star Wars in ASCII"
+    telnet towel.blinkenlights.nl; end
+
+function brewsync -d "Sync your installed tools"
+    ansible-playbook -i $HOME/.dotfiles/init/inventory $HOME/.dotfiles/init/dotfiles.yml --tags packages; end
+
+function dotfiles -d "Run dotfiles Playbook"
+    ansible-playbook -i $HOME/.dotfiles/init/inventory $HOME/.dotfiles/init/dotfiles.yml; end
+
+function play -d "Run an ansible playbook (alias)"
+    ansible-playbook $argv; end
+
+function s -d "Open in Sublime Text"
+    subl $argv; end
+
+function smod -d "Show permissions in decimal"
+    stat -c "%a %n"; end
+
+function ubuntu -d "Create a short lived Ubuntu container"
+    docker run -rm -ti ubuntu /bin/bash; end
--- config/fish/config.fish
+++ config/fish/config.fish
@@ -1,8 +1,24 @@
 # Path to Oh My Fish install.
 set -gx OMF_PATH $HOME/.local/share/omf
 
-# Customize Oh My Fish configuration path.
-#set -gx OMF_CONFIG $HOME/.dotfiles/config/omf
-
 # Load oh-my-fish configuration.
 source $OMF_PATH/init.fish
+
+# Paths
+test -d /usr/local/sbin; and set -x PATH /usr/local/sbin $PATH
+test -d /usr/local/bin ; and set -x PATH /usr/local/bin $PATH
+
+set -x EDITOR vim
+set -x VISUAL vim
+source $HOME/.dotfiles/config/fish/aliases.fish
+
+# Install brew casks in /Applications
+set -x HOMEBREW_CASK_OPTS "--appdir=/Applications"
+
+# docker-machine
+set -x DOCKER_TLS_VERIFY yes
+set -x DOCKER_CERT_PATH $HOME/.docker/machine/machines/dev
+set -x DOCKER_HOST tcp://192.168.99.100:2376
+
+# Load iTerm 2 shell integration
+test -e {$HOME}/.config/fish/iterm2_shell_integration.fish; and source {$HOME}/.config/fish/iterm2_shell_integration.fish
