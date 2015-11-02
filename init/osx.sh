7b8427ad7fd22e9c961a7e3f26b0df94a56f53ad
elnappo <elnappoo@gmail.com>
2015-11-02 17:30:26 +0100

    fix some defaults

--- init/osx.sh
+++ init/osx.sh
@@ -41,13 +41,6 @@ defaults write com.apple.finder FXInfoPanesExpanded -dict \
 	OpenWith -bool true \
 	Privileges -bool true
 
-# Disable automatic emoji substitution (i.e. use plain text smileys)
-defaults write com.apple.messageshelper.MessageController SOInputLineSettings -dict-add "automaticEmojiSubstitutionEnablediMessage" -bool false
-
-# Disable smart quotes as it’s annoying for messages that contain code
-defaults write com.apple.messageshelper.MessageController SOInputLineSettings -dict-add "automaticQuoteSubstitutionEnabled" -bool false
-
-
 ###############################################################################
 # Kill affected applications                                                  #
 ###############################################################################
